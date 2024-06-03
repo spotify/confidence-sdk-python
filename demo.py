f87032a9b7f5d927f0e38fdb78d38fb52acc4bb2
vahidlazio <vahidlazio@gmail.com>
2024-06-03 11:55:42 +0200

    fix: revert having run in the track event (#56)
    * revert having run in the track event
    
    * make track blocking

--- demo.py
+++ demo.py
@@ -5,11 +5,12 @@ from confidence.confidence import Confidence
 
 
 async def get_flag():
-    root = Confidence("API_KEY")
+    root = Confidence("API_CLIENT")
     random_uuid = uuid.uuid4()
     uuid_string = str(random_uuid)
     confidence = root.with_context({"targeting_key": uuid_string})
-    await confidence.with_context({"app": "python"}).track_async("navigate", {})
+    confidence.with_context({"app": "python"}).track("navigate", {})
+    print("Tracked navigate event")
 
     value = confidence.resolve_string_details("hawkflag.color", "False")
     print(f"Flag value: {value}")
@@ -19,10 +20,15 @@ async def get_flag():
 async def main():
     await get_flag()
     print("Finished calling get_flag")
-    await asyncio.sleep(1)
+    await asyncio.sleep(5)
     print("Finished sleeping for 1 seconds")
+
+
+def sync_main():
+    print("Running main asynchronously")
 
 
 # Run the main function using asyncio.run (Python 3.7+)
 if __name__ == "__main__":
     asyncio.run(main())
+    sync_main()
